f924ec11447cf0b0499e4fe991d07017f0838ecf
xiaoxuan huang <sq7@xiaoxuandeMacBook-Pro.local>
2021-11-05 02:03:37 +0100

    modification clilauncher

--- cli/src/main/java/up/visulog/cli/CLILauncher.java
+++ cli/src/main/java/up/visulog/cli/CLILauncher.java
@@ -74,6 +74,8 @@ public class CLILauncher {
                             });
                             if (pValue.equals("countCommitsPerHour")) plugins.put("countCommitsPerHour", new PluginConfig() {
                             });
+                            if (pValue.equals("dailyAverage")) plugins.put("dailyAverage", new PluginConfig() {
+                            });
                             break;
                         case "--loadConfigFile":
                             // TODO (load options froadd m a file)
