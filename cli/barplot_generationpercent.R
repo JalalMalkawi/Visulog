7e9fc7d2533aee2ac5b8d72338bf0f6387bb0a01
CHAUVEAU cyprien <cyprien.chauveau@orange.fr>
2021-11-08 20:41:59 +0100

    new version % graph generation

---
+++ cli/barplot_generationpercent.R
@@ -0,0 +1,65 @@
+# get data from file
+# column 1 is labels (row names)
+x <- read.table("./visulog/commitsPA.txt", header = FALSE, row.names = 1)
+colnames(x) <- "nb"
+attach(x)
+
+## SETTINGS:
+prob <- TRUE # set to TRUE if barplot(%) desired
+tofile <- TRUE # set to FALSE to print in RStudio
+
+nbauth <- data.frame(table(nb))
+
+nbauth
+
+pct <- 100*nb/sum(nb) # normalize to empirical probabilities (%)
+# build colors from %
+pcgood <- 10#0/nbauth # level qualifying what "good" means
+cl <- (pct < pcgood)*2 # red
+cl[cl == 0] <- 3   # green
+
+# Do the plot
+clab <- 1.4    # label size, 1 = default size
+cnames <- 0.8  # bar names size, adapt to nb of bars
+lgdbon <- paste(">", pcgood,"%")
+lgdbad <- paste("<", pcgood,"%")
+
+if (! tofile) if (prob) {
+  barplot(pct, names.arg = row.names(x), main="Commits par auteurs (%)",
+        ylim = c(0, min(c(100, max(pct)+10))),
+        xlab="auteurs", ylab="% de commits" , 
+        cex.names = cnames, cex.lab=clab, col=cl)
+  legend("topleft", legend = c(lgdbon,lgdbad), fill=3:2,
+       bg = "antiquewhite")
+  # visualize "good" level...
+  abline(h = pcgood, lty=2, col=8)
+} else {
+  barplot(nb, names.arg = row.names(x), main="Commits par auteurs",
+          xlab="auteurs", ylab="nombre de commits", 
+          cex.names = cnames, cex.lab=clab)}
+
+# output plot to a file in png; see ?pdf or ?png
+if (tofile) {
+  pdf(file = "mybarplot.pdf", width=7, height=5) # 1920/1080 full HD png
+  clab <- 1.4    # label size, 1 = default size
+  cnames <- 1  # bar names size, adapt to nb of bars
+  
+  if (prob) {
+    barplot(pct, names.arg = row.names(x), main="Commits par auteurs (%)",
+            ylim = c(0, min(c(100, max(pct)+10))),
+            xlab="auteurs", ylab="% de commits" , 
+            cex.names = cnames, cex.lab=clab, col=cl)
+    legend("topleft", legend = c(lgdbon,"mauvais"), fill=3:2,
+           bg = "antiquewhite")
+    # visualize "good" level...
+    abline(h = pcgood, lty=2, col=8)
+  } else {
+    barplot(nb, names.arg = row.names(x), main="Commits par auteurs",
+            xlab="auteurs", ylab="nombre de commits", 
+            cex.names = cnames, cex.lab=clab)}
+  
+  dev.off() # close file
+}
+########
+# pdf width=7, height=5
+
