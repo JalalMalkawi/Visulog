964b0334897dae7ec785db5fa7a506c765528d71
BENMOUFFEK jeremy <jeremybnfk@gmail.com>
2021-11-09 19:07:07 +0100

    correction du msg d'erreur SLF4J #40

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -9,6 +9,7 @@ application.mainClass.set("up.visulog.cli.CLILauncher")
 dependencies {
     implementation("org.eclipse.jgit:org.eclipse.jgit:5.4.2.201908231537-r")
     implementation("commons-io:commons-io:2.6")
+    implementation("org.slf4j", "slf4j-nop","1.7.29")
     implementation(project(":analyzer"))
     implementation(project(":config"))
     implementation(project(":gitrawdata"))
