46d6129bb60d4a1bdee631f185437c462035a767
CHAUVEAU cyprien <cyprien.chauveau@orange.fr>
2021-12-07 09:49:35 +0100

    C/month

--- analyzer/src/main/java/up/visulog/analyzer/RInvocation.java
+++ analyzer/src/main/java/up/visulog/analyzer/RInvocation.java
@@ -20,6 +20,7 @@ public class RInvocation{
     }
     public static void runWithR(String nomFichier)throws IOException{
         String s = nomFichier.substring(0,nomFichier.length()-2) + "Result.txt";
+        //System.out.println(nomFichier);
         new ProcessBuilder("R", "CMD" , "BATCH" , nomFichier , s).start();
     }
     
